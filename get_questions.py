d297288704eed0b726819200443133ee67ceeb6f
Pedro Simão Januário Vieira <up202108768@edu.fe.up.pt>
2024-10-04 20:10:49 +0100

    Initial question filtering
    Processed questions from parquet 0 to 16.

---
+++ get_questions.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+beginning = 11
+end = 16
+
+def get_questions(posts):
+    # PostTypeId only 1 (questions)
+    posts = posts.loc[posts['PostTypeId'] == 1]
+
+    # Only questions with last activity date greater than 2023-01-01
+    posts = posts.loc[(posts['LastActivityDate'] >= '2023-01-01') & (posts['LastActivityDate'] < '2023-02-01')]
+
+    #print(posts.head())
+    
+    return posts
+
+for i in range(beginning, end + 1):
+    if i == 8:
+        continue
+
+    path = f'dataset/stackoverflow-posts-{i:05}-of-00058.parquet'
+    print(f'Reading parquet {i}...')
+    temp = pd.read_parquet(path, engine='pyarrow')
+
+    # Remove columns 'ContentLicense', 'FavoriteCount', 'LastEditorUserId', 'OwnerUserId', 'ViewCount'
+    temp = temp.drop(columns=['ContentLicense', 'FavoriteCount', 'LastEditorUserId', 'OwnerUserId', 'ViewCount'])
+
+    print(f'Getting questions of parquet no {i}...')
+    
+    temp = get_questions(temp)
+
+    print(f'Finished getting questions of parquet {i}')
+    
+    temp.to_csv(f'questions.csv', index=False, mode='a')
+    print(f'Appended questions of parquet {i}\n')
+
+    # write last parquet no to last.txt
+    with open('last.txt', 'w') as f:
+        f.write(str(i))
+    
+    del temp
+
+#print(questions)
