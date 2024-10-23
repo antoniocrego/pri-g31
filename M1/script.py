de3ec405f8824e99feacbb776eaa235369da1e75
Pedro Simão Januário Vieira <up202108768@edu.fe.up.pt>
2024-10-23 18:09:25 +0100

    M2 init: get JSONs

--- M1/script.py
+++ M1/script.py
@@ -1,4 +1,4 @@
 
-# exec(open("questions/get_questions.py").read())
-#exec(open("answers/get_answers.py").read())
+exec(open("questions/get_questions.py").read())
+exec(open("answers/get_answers.py").read())
 exec(open("questions/filter_questions.py").read())
