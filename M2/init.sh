ba3899e61be95ad3fb26a914ac44d1c62a9bc63f
Pedro Simão Januário Vieira <up202108768@edu.fe.up.pt>
2024-11-07 09:58:53 +0000

    New schema and uploading scripts fixed

--- M2/init.sh
+++ M2/init.sh
@@ -1,9 +1,3 @@
 #! /bin/bash
 
-docker run -p 8983:8983 --name solr -v ${PWD}/dataset:/data -d solr:9 solr-precreate stackoverflow
-
-curl -X POST -H 'Content-type:application/json' --data-binary dataset/schema.json http://localhost:8983/solr/stackoverflow/schema
-
-docker exec solr bin/post -c stackoverflow /data/questions.json
-
-docker exec solr bin/post -c stackoverflow /data/answers.json
+docker run -p 8983:8983 --name solr -v ${PWD}/dataset:/data -d solr:9 #solr-precreate stackoverflow
