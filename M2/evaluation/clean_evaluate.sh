ea5fda3ec9a624f41f226e490085e626acb1a74b
up202108806 <up202108806@edu.fe.up.pt>
2024-11-16 11:43:19 +0000

    Create evaluate_full.sh and clean_evaluate.sh scripts

---
+++ M2/evaluation/clean_evaluate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm qrels1_trec.txt
+rm qrels2_trec.txt
+rm qrels3_trec.txt
+rm results_query1_schema_trec.txt
+rm results_query2_schema_trec.txt
+rm results_query3_schema_trec.txt
+rm results_query1_schemaless_trec.txt
+rm results_query2_schemaless_trec.txt
+rm results_query3_schemaless_trec.txt
