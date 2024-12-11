2b97116eaf0beff80ad99cdd62fa8f8ed1b8271d
Pedro Simão Januário Vieira <up202108768@edu.fe.up.pt>
2024-12-11 21:50:05 +0000

    About page

--- M3/frontend/pri-31/app/about/page.tsx
+++ M3/frontend/pri-31/app/about/page.tsx
@@ -5,11 +5,23 @@ export default function Page() {
   return (
     <div>
       <div className="mt-4 flex grow flex-col md:flex-row">
-        <div className="p-8 flex flex-col justify-center rounded-lg bg-gray-50 w-full md:px-20">
+        <div className="p-4 grid grid-cols-1 md:grid-cols-2 gap-4 justify-center rounded-lg bg-gray-50 w-full md:px-20">
+          <GroupMember
+            name='António Rego'
+            email='up202108666@up.pt'
+            photo='antonio'/>
+          <GroupMember
+            name='Daniel Bernardo'
+            email='up202108667@up.pt'
+            photo='daniel'/>
+          <GroupMember
+            name='Pedro Lima'
+            email='up202108806@up.pt'
+            photo='pedrolima'/>
           <GroupMember
             name='Pedro Januário'
             email='up202108768@up.pt'
-            photo='pedrojanu.jpg'/>
+            photo='pedrojanu'/>
         </div>
       </div>
     </div>
--- M3/frontend/pri-31/app/ui/group-member.tsx
+++ M3/frontend/pri-31/app/ui/group-member.tsx
@@ -14,17 +14,21 @@ export default function GroupMember(
         photo: string;
     }
 ) {
-    console.log(photo);
-  return (
+    const photoPath = `/${photo}.jpg`;
+    return (
     <div>
         <div className="mt-4 flex grow flex-col md:flex-row">
-            <div className="p-8 flex flex-col justify-center rounded-lg bg-gray-50 w-full md:px-20">
-                <Image src={`@/public/${photo.toString()}`} alt={name} width={200} height={200} />
-                <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-                    <strong>{name}</strong>
-                    <br></br>
-                    <a href={`mailto:${email}`} className="text-blue-500">{email}</a>
-                </p>
+            <div className="p-8 flex flex-col md:flex-row justify-center rounded-lg bg-gray-50 w-full md:px-20">
+                <div className="md:mr-8">
+                    <Image src={photoPath} alt={name} width={200} height={200} />
+                </div>
+                <div className="p-4 flex flex-col justify-center">
+                    <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+                        <strong>{name}</strong>
+                        <br></br>
+                        <a href={`mailto:${email}`} className="text-blue-500">{email}</a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
