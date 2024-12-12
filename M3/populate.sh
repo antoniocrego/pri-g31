67ee122c0be46beec04d7518e98e87c11d973091
Daniel Cabral Bernardo <up202108667@edu.fe.up.pt>
2024-12-12 09:31:36 +0000

    Added semantic search

--- M3/populate.sh
+++ M3/populate.sh
@@ -3,5 +3,5 @@
 docker exec solr bin/solr delete -c stackoverflow
 docker exec solr bin/solr create -c stackoverflow
 curl -X POST -H 'Content-type:application/json' --data-binary '@dataset/schema.json' http://localhost:8983/solr/stackoverflow/schema
-docker exec solr bin/post -c stackoverflow /data/questions.json
+docker exec solr bin/post -c stackoverflow /data/questions_with_embeddings.json
 # docker exec solr bin/post -c stackoverflow /data/answers.json
