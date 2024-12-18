d5cd0749fcf2239df14a0aa62649d3dd007dd797
Pedro Simão Januário Vieira <up202108768@up.pt>
2024-12-18 10:41:47 +0000

    Evaluate v2

--- M3/solr/evaluation/clean_evaluate.sh
+++ M3/solr/evaluation/clean_evaluate.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-rm qrels1_trec.txt
-rm qrels2_trec.txt
-rm qrels3_trec.txt
-rm results_query1_schema_trec.txt
-rm results_query2_schema_trec.txt
-rm results_query3_schema_trec.txt
-rm results_query1_schemaless_trec.txt
-rm results_query2_schemaless_trec.txt
-rm results_query3_schemaless_trec.txt
-rm eval_query1_schema.txt
-rm eval_query2_schema.txt
-rm eval_query3_schema.txt
-rm eval_query1_schemaless.txt
-rm eval_query2_schemaless.txt
-rm eval_query3_schemaless.txt
-rm prec_rec_query1_schema.png
-rm prec_rec_query2_schema.png
-rm prec_rec_query3_schema.png
-rm prec_rec_query1_schemaless.png
-rm prec_rec_query2_schemaless.png
-rm prec_rec_query3_schemaless.png
+rm eval_query1.txt
+rm eval_query2.txt
+rm eval_query3.txt
+rm prec_rec_query1.png
+rm prec_rec_query2.png
+rm prec_rec_query3.png
