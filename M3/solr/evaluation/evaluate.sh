dca9eaf5a09d0de72f652d31cceb81fa750b48c5
Pedro Simão Januário Vieira <up202108768@edu.fe.up.pt>
2024-12-18 11:09:24 +0000

    Semantic eval v1

--- M3/solr/evaluation/evaluate.sh
+++ M3/solr/evaluation/evaluate.sh
@@ -4,6 +4,10 @@
 ./scripts/query_solr.py --query config/query2.json --uri http://localhost:8983/solr --collection stackoverflow | ./scripts/solr2trec.py > results_query2_trec.txt
 ./scripts/query_solr.py --query config/query3.json --uri http://localhost:8983/solr --collection stackoverflow | ./scripts/solr2trec.py > results_query3_trec.txt
 
+cat return_semantic_query1.json | ./scripts/solr2trec.py > results_semantic_query1_trec.txt
+cat return_semantic_query2.json | ./scripts/solr2trec.py > results_semantic_query2_trec.txt
+cat return_semantic_query3.json | ./scripts/solr2trec.py > results_semantic_query3_trec.txt
+
 cat config/qrels1.txt | ./scripts/qrels2trec.py > qrels1_trec.txt
 cat config/qrels2.txt | ./scripts/qrels2trec.py > qrels2_trec.txt
 cat config/qrels3.txt | ./scripts/qrels2trec.py > qrels3_trec.txt
@@ -17,11 +21,22 @@ src/trec_eval/trec_eval qrels2_trec.txt results_query2_trec.txt > eval_query2.tx
 echo " --- Evaluating for Query 3 --- "
 src/trec_eval/trec_eval qrels3_trec.txt results_query3_trec.txt > eval_query3.txt
 
+echo " --- Evaluating for Semantic Query 1 --- "
+src/trec_eval/trec_eval qrels1_trec.txt results_semantic_query1_trec.txt > eval_semantic_query1.txt
+
+echo " --- Evaluating for Semantic Query 2 --- "
+src/trec_eval/trec_eval qrels2_trec.txt results_semantic_query2_trec.txt > eval_semantic_query2.txt
+
+echo " --- Evaluating for Semantic Query 3 --- "
+src/trec_eval/trec_eval qrels3_trec.txt results_semantic_query3_trec.txt > eval_semantic_query3.txt
+
 cat results_query1_trec.txt | ./scripts/plot_pr.py --qrels qrels1_trec.txt --output prec_rec_query1.png
-
 cat results_query2_trec.txt | ./scripts/plot_pr.py --qrels qrels2_trec.txt --output prec_rec_query2.png
-
 cat results_query3_trec.txt | ./scripts/plot_pr.py --qrels qrels3_trec.txt --output prec_rec_query3.png
 
+cat results_semantic_query1_trec.txt | ./scripts/plot_pr.py --qrels qrels1_trec.txt --output prec_rec_semantic_query1.png
+cat results_semantic_query2_trec.txt | ./scripts/plot_pr.py --qrels qrels2_trec.txt --output prec_rec_semantic_query2.png
+cat results_semantic_query3_trec.txt | ./scripts/plot_pr.py --qrels qrels3_trec.txt --output prec_rec_semantic_query3.png
+
 rm qrels1_trec.txt qrels2_trec.txt qrels3_trec.txt
-rm results_query1_trec.txt results_query2_trec.txt results_query3_trec.txt
+rm results_query1_trec.txt results_query2_trec.txt results_query3_trec.txt results_semantic_query1_trec.txt results_semantic_query2_trec.txt results_semantic_query3_trec.txt
